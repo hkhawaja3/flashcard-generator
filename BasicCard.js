43e9a3bae78be97b5124aec69a4df5d660ed35d9
Hamza Khawaja <hkhawaja3@gmail.com>
2017-09-14 14:01:50 -0500

    Update BasicCard.js

--- BasicCard.js
+++ BasicCard.js
@@ -1,1 +1,12 @@
+function BasicCard(answer, question){
+	if (this instanceof BasicCard) {
+        this.type = 'basic';
+        this.front = question;
+        this.back = answer;
+    }
+    else {
+        return new BasicCard(answer, question);
+    }
+}
 
+module.exports = BasicCard;
