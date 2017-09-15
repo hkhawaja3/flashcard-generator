04155e9b8b783a28e01b3740163b77d719966f5c
Hamza Khawaja <hkhawaja3@gmail.com>
2017-09-15 10:03:40 -0500

    Update ClozeCard.js

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,1 +1,14 @@
 
+function ClozeCard(answer, question) {
+    if (this instanceof ClozeCard) {
+        this.type = 'cloze';
+        this.partial = question.replace(answer, '...');
+        this.fullText = question;
+        this.cloze = answer;
+    } else {
+        return new ClozeCard(answer, question);
+
+    }
+}
+
+module.exports = ClozeCard;
